c5e09919733687601d78433075a9ec17053b9861
lizhenghui <lizhh@yilan.tv>
2019-01-26 16:00:50 +0800

    client

--- src/YLYun/Client.php
+++ src/YLYun/Client.php
@@ -1,12 +1,44 @@
 <?php
+/**
+ * 一览视频云 客户端类
+ * 视频类型：
+ * 短视频(shortVideo)：一般时长在1分钟左右的横版视频
+ * 小视频(microVideo)：一般时长在10秒左右的竖版视频
+ */
 namespace YLYun;
 
 class Client {
+
+	private $_key;
+	private $_token;
 
 
 	public function __construct() {
 
 	}
 
-    public function recommend() { }
+	//获取频道
+    public function getChannel() {
+
+    }
+
+	//频道推荐
+    public function recommend() {
+
+    }
+
+    //携带广告的频道推荐
+    public function recommendWithAd() {
+
+    }
+
+	//视频详情
+    public function videoDetail() {
+
+    }
+
+	//视频相关
+    public function videoRelate() {
+
+    }
 }
