a227f85ca8dd90ed22b2361d8555339f4e521b10
lizhenghui <lizhh@yilan.tv>
2019-02-21 21:16:15 +0800

    bugfix

--- src/YLYun/Config.php
+++ src/YLYun/Config.php
@@ -5,9 +5,9 @@ namespace YLYun;
 class Config {
 
     //以下常量上线前需要修改 请咨询一览接口人索取
-    const ACCESS_KEY    = 'ylx36jukc8oq';
-    const ACCESS_TOKEN  = 'ow5um6c233cax89dyuaqzh3g3l4duxdx';
-    const PLATFORM      = 'bjqm'; //渠道名称
+    const ACCESS_KEY    = '';
+    const ACCESS_TOKEN  = '';
+    const PLATFORM      = ''; //渠道名称
     const ENV           = 'prod';  //环境 dev/prod
     const HOST_PROD     = 'https://openapiv2.yilan.tv'; //线上正式域名
 
