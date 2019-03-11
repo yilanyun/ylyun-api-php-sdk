3bda4007718f0581ae5829b2f2a45631e9e88638
lizhenghui <lizhh@yilan.tv>
2019-03-11 19:54:39 +0800

    下线接口

--- src/YLYun/Video.php
+++ src/YLYun/Video.php
@@ -15,7 +15,6 @@ class Video {
 	public static $urls = [
 		'detail' => '/video/detail',
 		'relate' => '/video/relation',
-		'play' => '/video/play',
 	];
 
 	public function __construct($client) {
@@ -55,23 +54,6 @@ class Video {
     	$res = Http::get($this->client, $url);
     	if ($res['code'] == '200' && $res['data']) {
     		return $res['data'];
-    	} else {
-    		throw new YLYunException($res['msg'], $res['code']);
-    	}
-    }
-
-    /**
-     * 视频播放
-     * @param string $vid 视频ID
-	 * @return array
-     */
-    public function videoPlay($vid) {
-    	$input['id'] = $vid;
-    	$this->params = array_merge($this->common, $input);
-    	$url = Tools::getFullUrl(self::$urls['play'], $this->params);
-    	$res = Http::get($this->client, $url);
-    	if ($res['code'] == '200' && $res['bitrates']) {
-    		return $res['bitrates'];
     	} else {
     		throw new YLYunException($res['msg'], $res['code']);
     	}
